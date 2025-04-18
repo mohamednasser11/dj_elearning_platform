c694c40f8bd06ffe26227405bae642e739c1c057
mohamednasser11 <mohamednaser1000.mn@gmail.com>
2025-04-19 00:14:00 +0200

    adding the generate questions from pdf fils

--- tools/file_summarization/file_models.py
+++ tools/file_summarization/file_models.py
@@ -1,16 +1,19 @@
-import uuid
 from django.db import models
+from ...users.models.user_model import User
+
 
 class FileUploadModel(models.Model):
     fileId = models.UUIDField(primary_key=True, editable=False)
-    file = models.FileField(upload_to='files/')
+    file = models.FileField(upload_to="files/")
     fileName = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="file_uploads")
+    is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.file_name
-    
+
     class Meta:
-        verbose_name = 'file Upload'
-        verbose_name_plural = 'files Upload'
+        verbose_name = "file Upload"
+        verbose_name_plural = "files Upload"
--- tools/urls.py
+++ tools/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .file_summarization.file_view import fileSummerizationView
+from .file_summarization.file_view import FileSummerizationView, QuestiouGenerationView
 
 
 urlpatterns = [
-    path("file-summarization/", fileSummerizationView.as_view(), name="file-summarization"),
+    path("file-summarization/", FileSummerizationView.as_view(), name="file-summarization"),
+    path("file-summarization/<fileId>", FileSummerizationView.as_view(), name="update-delete-file"),
+    path("question-generation/", QuestiouGenerationView.as_view(), name="question-generation"),
 ]
