b96d51ac9aa7611d182e22ad9c3ba174736f162a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-06-03 18:16:02 +0300

    feat: add context

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,17 @@
 import json
+from weakref import WeakValueDictionary
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import asyncio
 from tools.AI.AI_model_service import AIModelService
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    active_clients = WeakValueDictionary()
+
     async def connect(self):
         await self.accept()
+        self.user = self.scope["user"]
+        self.history = []
 
     async def disconnect(self, code):
         pass
@@ -25,21 +30,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 return
 
             text_data_json = json.loads(text_data)
-            message = text_data_json.get("message", "")
+            inbound_message = text_data_json.get("message", "")
 
-            if not message.strip():
+            if not inbound_message.strip():
                 await send("error", "Empty message")
                 await self.close(4000)
                 return
 
+            self.history.append({"role": "user", "content": inbound_message})
+
+            print(self.history)
             model = AIModelService()
-            stream = model.chat(message)
+            stream = model.chat(self.history)
 
             await send("started")
 
+            outbound_message = ""
             for chunk in stream:
-                await send("streaming", chunk["message"]["content"])
+                response = chunk["message"]["content"]
+                outbound_message += response
+                await send("streaming", response)
                 await asyncio.sleep(0)  # to force yield
+
+            self.history.append({"role": "assistant", "content": outbound_message})
 
             await send("complete")
 
