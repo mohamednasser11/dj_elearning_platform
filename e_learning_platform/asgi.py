c2cf1f3f07a113169085fb8f8486a7a61936238d
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-06-04 16:05:44 +0300

    feat: add sessions

--- e_learning_platform/asgi.py
+++ e_learning_platform/asgi.py
@@ -10,12 +10,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from chat.urls import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "e_learning_platform.settings")
 
 
 def get_websocket_application():
+    from chat.urls import websocket_urlpatterns
     from middleware.websocket_auth_middleware import WebSocketJWTAuthMiddleware
 
     return WebSocketJWTAuthMiddleware(URLRouter(websocket_urlpatterns))
