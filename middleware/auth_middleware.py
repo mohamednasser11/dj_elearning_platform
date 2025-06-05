6ce39ee33a52fbc5f7453a423e8620e04a2950bd
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-06-05 18:51:24 +0300

    feat: whitelist admin pages

--- middleware/auth_middleware.py
+++ middleware/auth_middleware.py
@@ -1,6 +1,6 @@
-from rest_framework import status 
+from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.exceptions import AuthenticationFailed 
+from rest_framework.exceptions import AuthenticationFailed
 from .config import VALID_URLS
 from django.http import JsonResponse
 
@@ -10,31 +10,38 @@ class AuthMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if request.path.startswith("/admin/"):
+            return self.get_response(request)
+
         try:
             self.auth_check(request)
         except AuthenticationFailed as e:
             return self.unauthorized_response(e)
-        
+
         response = self.get_response(request)
         return response
 
     def auth_check(self, request):
-        if (
-            request.path not in VALID_URLS
-            and ("Authorization" not in request.headers
-            or not self.is_token_valid(request))
+        if request.path not in VALID_URLS and (
+            "Authorization" not in request.headers or not self.is_token_valid(request)
         ):
             print(f"Method ==> {request.path}")
             raise AuthenticationFailed("UnAuthorized Access!", code="unauthorized")
-        
+
     def is_token_valid(self, request):
         JWTAuthenticator = JWTAuthentication()
-        [bearer, unValidated_token] = request.headers["Authorization"].split(' ') if "Authorization" in request.headers else None
+        [bearer, unValidated_token] = (
+            request.headers["Authorization"].split(" ")
+            if "Authorization" in request.headers
+            else None
+        )
         print(f"Unvalidated Token: {unValidated_token}")
         decoded_token = JWTAuthenticator.get_validated_token(unValidated_token)
         print(f"decoded_token: {decoded_token}")
 
         return decoded_token is not None
-        
+
     def unauthorized_response(self, message):
-        return JsonResponse({"error": str(message)}, status=status.HTTP_401_UNAUTHORIZED)
+        return JsonResponse(
+            {"error": str(message)}, status=status.HTTP_401_UNAUTHORIZED
+        )
