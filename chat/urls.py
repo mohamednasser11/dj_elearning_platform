edaa143945e2c06d9527bfbab2be2bb7ef7e9b69
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-06-09 20:20:16 +0300

    feat: add endpoint to get chat sessions

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
+from chat.views import ChatView
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
     path("ws/chat/", ChatConsumer.as_asgi()),
+]
+
+urlpatterns = [
+    path("<int:courseId>/", ChatView.as_view(), name="get-chats-for-course"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from chat.models import ChatSession
+from departments.models.courses_models.course_models import Course
 
-# Create your views here.
+
+class ChatSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatSession
+        fields = ["id", "created_at", "updated_at"]
+
+
+class ChatView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, courseId):
+        try:
+            if courseId:
+                course = Course.objects.get(courseId=courseId)
+                sessions = ChatSession.objects.filter(user=request.user, course=course)
+                print(list(sessions))
+                serialized_sessions = []
+                for session in sessions:
+                    serialized_sessions.append(ChatSessionSerializer(session).data)
+                return Response(
+                    serialized_sessions,
+                    status=status.HTTP_200_OK,
+                )
+
+        except Course.DoesNotExist:
+            return Response(
+                {"error": "Course not found"}, status=status.HTTP_404_NOT_FOUND
+            )
