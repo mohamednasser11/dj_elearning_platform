4ff538bd3ad792656122f693a4a41c456d399245
Amedo <amedoeyes@gmail.com>
2025-06-20 17:59:45 +0300

    feat/course-creation (#35)
    * feat: add public path parents to auth middle ware
    
    * feat: add path to serve files
    
    * fix: lesson creation path
    
    * fix: lesson view request immutable data
    
    * feat: change lesson video save location
    
    * feat: improve course model and course creation view

--- departments/courses_urls.py
+++ departments/courses_urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('purchase/<int:courseId>/<int:userId>/', PurchaseCoursesView.as_view(), name='purchase-course'),
     path('getDepartmentCourses/<int:departmentId>/', getDepartmentCourses.as_view(), name='get-department-courses'),
     path('<int:courseId>/lesson/<int:lessonId>/', LessonsView.as_view(), name='create-delete-edit-lessons'),
-    path('<int:courseId>/', LessonsView.as_view(), name='patch-department-courses'),
+    path('<int:courseId>/lesson/', LessonsView.as_view(), name='patch-department-courses'),
 ]
